bccf2d67422096b8dbe7d8576e05c877d3680b88
SclShang <scl.shang@gmail.com>
2013-09-25 08:47:08 +0800

    Initial Commit

---
+++ DailyCost/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DailyCost
+//
+//  Created by Scliang on 13-9-25.
+//  Copyright (c) 2013年 ChuanliangShang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
